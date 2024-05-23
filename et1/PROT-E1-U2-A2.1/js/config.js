2ac3598eaa7aec41095be63c5549efc0f058545d
Vando Pinto <vandopintoo@gmail.com>
2024-05-23 17:30:29 -0300

    primeiras aulas

--- et1/PROT-E1-U2-A2.1/js/config.js
+++ et1/PROT-E1-U2-A2.1/js/config.js
@@ -12,59 +12,89 @@ var conclusao = [];
 //Array que possui a listagem de todos os items exitentes
 var items = [];
 
+var nomeUnidade = "";
+var nomeAula = "";
+
 
 items.push({
+    'titulo': 'OBJETIVOS DE APRENDIZAGEM',
     'nome': 'Aula 1',
     'arquivo': '1.html',
     'identificador': '0'
 });
 
 items.push({
+    'titulo': 'ESTRUTURA DA PELE',
     'nome': 'Aula 2',
     'arquivo': '2.html',
     'identificador': '1'
 });
 
 items.push({
+    'titulo': '',
     'nome': 'Aula 3',
     'arquivo': '4.html',
     'identificador': '3'
 });
 items.push({
+    'titulo': '',
     'nome': 'Aula 4',
     'arquivo': '5.html',
     'identificador': '4'
 });
 items.push({
+    'titulo': '',
     'nome': 'Aula 5',
     'arquivo': '6.html',
     'identificador': '5'
 });
 items.push({
+    'titulo': 'EPIDERME',
     'nome': 'Aula 6',
     'arquivo': '7.html',
     'identificador': '6'
 });
 items.push({
+    'titulo': '',
     'nome': 'Aula 7',
     'arquivo': '8.html',
     'identificador': '7'
 });
 
 
-items.push({'nome': 'Aula 8', 'arquivo': '9.html', 'identificador': '8'});
-items.push({'nome': 'Aula 9', 'arquivo': '10.html', 'identificador': '9'});
-items.push({'nome': 'Aula 10', 'arquivo': '11.html', 'identificador': '10'});
-items.push({'nome': 'Aula 11', 'arquivo': '12.html', 'identificador': '11'});
-items.push({'nome': 'Aula 12', 'arquivo': '13.html', 'identificador': '12'});
-items.push({'nome': 'Aula 13', 'arquivo': '14.html', 'identificador': '13'});
-items.push({'nome': 'Aula 14', 'arquivo': '15.html', 'identificador': '14'});
-items.push({'nome': 'Aula 15', 'arquivo': '16.html', 'identificador': '15'});
+items.push({
+    'titulo': '', 'nome': 'Aula 8', 'arquivo': '9.html', 'identificador': '8'
+});
+items.push({
+    'titulo': 'DERME', 'nome': 'Aula 9', 'arquivo': '10.html', 'identificador': '9'
+});
+items.push({
+    'titulo': '', 'nome': 'Aula 10', 'arquivo': '11.html', 'identificador': '10'
+});
+items.push({
+    'titulo': 'HIPODERME OU TECIDO SUBCUTÂNEO', 'nome': 'Aula 11', 'arquivo': '12.html', 'identificador': '11'
+});
+items.push({
+    'titulo': '', 'nome': 'Aula 12', 'arquivo': '13.html', 'identificador': '12'
+});
+items.push({
+    'titulo': '', 'nome': 'Aula 13', 'arquivo': '14.html', 'identificador': '13'
+});
+items.push({
+    'titulo': '', 'nome': 'Aula 14', 'arquivo': '15.html', 'identificador': '14'
+});
+items.push({
+    'titulo': 'REFERÊNCIAS', 'nome': 'Aula 15', 'arquivo': '16.html', 'identificador': '15'
+});
 
 
 
-items.push({ 'nome': 'Credito', 'arquivo': 'credito1.html', 'identificador': 'c1'});
-items.push({ 'nome': 'Credito', 'arquivo': 'credito2.html', 'identificador': 'c2'});
+items.push({
+    'titulo': 'FICHA TÉCNICA', 'nome': 'Credito', 'arquivo': 'credito1.html', 'identificador': 'c1'
+});
+items.push({
+    'titulo': '', 'nome': 'Credito', 'arquivo': 'credito2.html', 'identificador': 'c2'
+});
 
 
 
