9d66eec189b58ddcd0a1cbc7b925cd3bb921a9a3
Vando Pinto <vandopintoo@gmail.com>
2024-05-24 11:35:14 -0300

    ajsutes

--- et1/PROT-E1-U3-A3.1/js/config.js
+++ et1/PROT-E1-U3-A3.1/js/config.js
@@ -17,66 +17,102 @@ var nomeAula = "Aula 3.1 - Avaliação das Pessoas com Risco de Úlceras Crônic
 
 
 items.push({
+    'titulo': 'OBJETIVOS DE APRENDIZAGEM',
     'nome': 'Aula 1',
     'arquivo': '1.html',
     'identificador': '0'
 });
 
 items.push({
+    'titulo': 'BOAS-VINDAS!',
     'nome': 'Aula 2',
     'arquivo': '2.html',
     'identificador': '1'
 });
 items.push({
+    'titulo': 'AVALIAÇÃO DAS PESSOAS COM RISCO DE ÚLCERAS CRÔNICAS',
     'nome': 'Aula 3',
     'arquivo': '3.html',
     'identificador': '2'
 });
 items.push({
+    'titulo': '',
     'nome': 'Aula 4',
     'arquivo': '4.html',
     'identificador': '3'
 });
 items.push({
+    'titulo': 'MEDIDAS PREVENTIVAS DAS LESÕES POR PRESSÃO',
     'nome': 'Aula 5',
     'arquivo': '5.html',
     'identificador': '4'
 });
 items.push({
+    'titulo': 'AVALIAÇÃO DO RISCO',
     'nome': 'Aula 6',
     'arquivo': '6.html',
     'identificador': '5'
 });
 items.push({
+    'titulo': 'AVALIAÇÃO DA PELE',
     'nome': 'Aula 7',
     'arquivo': '7.html',
     'identificador': '6'
 });
 items.push({
+    'titulo': 'AVALIAÇÃO NUTRICIONAL',
     'nome': 'Aula 8',
     'arquivo': '8.html',
     'identificador': '7'
 });
 
 
-items.push({'nome': 'Aula 9', 'arquivo': '9.html', 'identificador': '8'});
-items.push({'nome': 'Aula 10', 'arquivo': '10.html', 'identificador': '9'});
-items.push({'nome': 'Aula 11', 'arquivo': '11.html', 'identificador': '10'});
-items.push({'nome': 'Aula 12', 'arquivo': '12.html', 'identificador': '11'});
-items.push({'nome': 'Aula 13', 'arquivo': '13.html', 'identificador': '12'});
-items.push({'nome': 'Aula 14', 'arquivo': '14.html', 'identificador': '13'});
-items.push({'nome': 'Aula 15', 'arquivo': '15.html', 'identificador': '14'});
-items.push({'nome': 'Aula 16', 'arquivo': '16.html', 'identificador': '15'});
-items.push({'nome': 'Aula 17', 'arquivo': '17.html', 'identificador': '16'});
-items.push({'nome': 'Aula 18', 'arquivo': '18.html', 'identificador': '17'});
-items.push({'nome': 'Aula 19', 'arquivo': '19.html', 'identificador': '18'});
-items.push({'nome': 'Aula 20', 'arquivo': '20.html', 'identificador': '19'});
+items.push({
+    'titulo': 'MANEJO DA UMIDADE', 'nome': 'Aula 9', 'arquivo': '9.html', 'identificador': '8'
+});
+items.push({
+    'titulo': 'MEDIDAS PREVENTIVAS DAS ÚLCERAS NOS PÉS POR DIABETES', 'nome': 'Aula 10', 'arquivo': '10.html', 'identificador': '9'
+});
+items.push({
+    'titulo': '', 'nome': 'Aula 11', 'arquivo': '11.html', 'identificador': '10'
+});
+items.push({
+    'titulo': '', 'nome': 'Aula 12', 'arquivo': '12.html', 'identificador': '11'
+});
+items.push({
+    'titulo': 'MEDIDAS PREVENTIVAS PARA ÚLCERA VENOSA', 'nome': 'Aula 13', 'arquivo': '13.html', 'identificador': '12'
+});
+items.push({
+    'titulo': 'CLASSIFICAÇÃO DO CEAP', 'nome': 'Aula 14', 'arquivo': '14.html', 'identificador': '13'
+});
+items.push({
+    'titulo': 'MEDIDAS PREVENTIVAS PARA ÚLCERA ISQUÊMICA', 'nome': 'Aula 15', 'arquivo': '15.html', 'identificador': '14'
+});
+items.push({
+    'titulo': '', 'nome': 'Aula 16', 'arquivo': '16.html', 'identificador': '15'
+});
+items.push({
+    'titulo': 'GUIA PARA AVALIAÇÃO DAS ÚLCERAS', 'nome': 'Aula 17', 'arquivo': '17.html', 'identificador': '16'
+});
+items.push({
+    'titulo': 'AVALIAÇÃO', 'nome': 'Aula 18', 'arquivo': '18.html', 'identificador': '17'
+});
+items.push({
+    'titulo': 'REFERÊNCIAS', 'nome': 'Aula 19', 'arquivo': '19.html', 'identificador': '18'
+});
+items.push({
+    'titulo': '', 'nome': 'Aula 20', 'arquivo': '20.html', 'identificador': '19'
+});
 
 
 
 
-items.push({ 'nome': 'Credito', 'arquivo': 'credito1.html', 'identificador': 'c1'});
-items.push({ 'nome': 'Credito', 'arquivo': 'credito2.html', 'identificador': 'c2'});
+items.push({
+    'titulo': 'FICHA TÉCNICA', 'nome': 'Credito', 'arquivo': 'credito1.html', 'identificador': 'c1'
+});
+items.push({
+    'titulo': '', 'nome': 'Credito', 'arquivo': 'credito2.html', 'identificador': 'c2'
+});
 
 
 
