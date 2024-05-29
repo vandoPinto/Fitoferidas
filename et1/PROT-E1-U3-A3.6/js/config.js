3850a98d0b60c64dfd6c955af3c918894c79fe5a
Vando Pinto <vandopintoo@gmail.com>
2024-05-29 18:36:43 -0300

    ajustes e aula 3.8

--- et1/PROT-E1-U3-A3.6/js/config.js
+++ et1/PROT-E1-U3-A3.6/js/config.js
@@ -16,21 +16,21 @@ var nomeUnidade = "Unidade 3 - Princípios da Prevenção e Tratamento de Ferida
 var nomeAula = "Aula 3.6 - Coberturas";
 
 
-items.push({'nome': 'Aula 1',    'arquivo': '1.html',    'identificador': '0'});
-items.push({'nome': 'Aula 2',    'arquivo': '2.html',    'identificador': '1'});
-items.push({'nome': 'Aula 3',    'arquivo': '3.html',    'identificador': '2'});
-items.push({    'nome': 'Aula 4',    'arquivo': '4.html',    'identificador': '3'});
-items.push({    'nome': 'Aula 5',    'arquivo': '5.html',    'identificador': '4'});
-items.push({    'nome': 'Aula 6',    'arquivo': '6.html',    'identificador': '5'});
-items.push({    'nome': 'Aula 7',    'arquivo': '7.html',    'identificador': '6'});
-items.push({    'nome': 'Aula 8',    'arquivo': '8.html',    'identificador': '7'});
-items.push({'nome': 'Aula 9', 'arquivo': '9.html', 'identificador': '8'});
-items.push({'nome': 'Aula 10', 'arquivo': '10.html', 'identificador': '9'});
-items.push({'nome': 'Aula 11', 'arquivo': '11.html', 'identificador': '10'});
+items.push({ 'titulo': 'OBJETIVOS DE APRENDIZAGEM', 'nome': 'Aula 1', 'arquivo': '1.html', 'identificador': '0' });
+items.push({ 'titulo': 'BOAS-VINDAS!', 'nome': 'Aula 2', 'arquivo': '2.html', 'identificador': '1' });
+items.push({ 'titulo': 'PRÉ-REQUISITO', 'nome': 'Aula 3', 'arquivo': '3.html', 'identificador': '2' });
+items.push({ 'titulo': 'INTRODUÇÃO', 'nome': 'Aula 4', 'arquivo': '4.html', 'identificador': '3' });
+items.push({ 'titulo': '', 'nome': 'Aula 5', 'arquivo': '5.html', 'identificador': '4' });
+items.push({ 'titulo': '', 'nome': 'Aula 6', 'arquivo': '6.html', 'identificador': '5' });
+items.push({ 'titulo': 'PRINCIPAIS COBERTURAS PARA O TRATAMENTO DE FERIDAS', 'nome': 'Aula 7', 'arquivo': '7.html', 'identificador': '6' });
+items.push({ 'titulo': '', 'nome': 'Aula 8', 'arquivo': '8.html', 'identificador': '7' });
+items.push({ 'titulo': 'CONSIDERAÇÕES FINAIS', 'nome': 'Aula 9', 'arquivo': '9.html', 'identificador': '8' });
+items.push({ 'titulo': 'AVALIAÇÃO', 'nome': 'Aula 10', 'arquivo': '10.html', 'identificador': '9' });
+items.push({ 'titulo': 'REFERÊNCIAS', 'nome': 'Aula 11', 'arquivo': '11.html', 'identificador': '10' });
 
 
-items.push({ 'nome': 'Credito', 'arquivo': 'credito1.html', 'identificador': 'c1'});
-items.push({ 'nome': 'Credito', 'arquivo': 'credito2.html', 'identificador': 'c2'});
+items.push({ 'titulo': 'FICHA TÉCNICA', 'nome': 'Credito', 'arquivo': 'credito1.html', 'identificador': 'c1' });
+items.push({ 'titulo': '', 'nome': 'Credito', 'arquivo': 'credito2.html', 'identificador': 'c2' });
 
 
 
--- et1/PROT-E1-U3-A3.7/js/config.js
+++ et1/PROT-E1-U3-A3.7/js/config.js
@@ -16,33 +16,33 @@ var nomeUnidade = "Unidade 3 - Princípios da Prevenção e Tratamento de Ferida
 var nomeAula = "Aula 3.7 - Infecção em Feridas Agudas e Crônicas";
 
 
-items.push({'nome': 'Aula 1', 'arquivo': '1.html', 'identificador': '0'});
-items.push({'nome': 'Aula 2', 'arquivo': '2.html', 'identificador': '1'});
-items.push({'nome': 'Aula 3', 'arquivo': '3.html', 'identificador': '2'});
-items.push({'nome': 'Aula 4', 'arquivo': '4.html', 'identificador': '3'});
-items.push({'nome': 'Aula 5', 'arquivo': '5.html', 'identificador': '4'});
-items.push({'nome': 'Aula 6', 'arquivo': '6.html', 'identificador': '5'});
-items.push({'nome': 'Aula 7', 'arquivo': '7.html', 'identificador': '6'});
-items.push({'nome': 'Aula 8', 'arquivo': '8.html', 'identificador': '7'});
-items.push({'nome': 'Aula 9', 'arquivo': '9.html', 'identificador': '8'});
-items.push({'nome': 'Aula 10', 'arquivo': '10.html', 'identificador': '9'});
-items.push({'nome': 'Aula 11', 'arquivo': '11.html', 'identificador': '10'});
-items.push({'nome': 'Aula 12', 'arquivo': '12.html', 'identificador': '11'});
-items.push({'nome': 'Aula 13', 'arquivo': '13.html', 'identificador': '12'});
-items.push({'nome': 'Aula 14', 'arquivo': '14.html', 'identificador': '13'});
-items.push({'nome': 'Aula 15', 'arquivo': '15.html', 'identificador': '14'});
-items.push({'nome': 'Aula 16', 'arquivo': '16.html', 'identificador': '15'});
-items.push({'nome': 'Aula 17', 'arquivo': '17.html', 'identificador': '16'});
-items.push({'nome': 'Aula 18', 'arquivo': '18.html', 'identificador': '17'});
-items.push({'nome': 'Aula 19', 'arquivo': '19.html', 'identificador': '18'});
-items.push({'nome': 'Aula 20', 'arquivo': '20.html', 'identificador': '19'});
-items.push({'nome': 'Aula 21', 'arquivo': '21.html', 'identificador': '20'});
-items.push({'nome': 'Aula 22', 'arquivo': '22.html', 'identificador': '21'});
+items.push({ 'titulo': 'OBJETIVOS DE APRENDIZAGEM', 'nome': 'Aula 1', 'arquivo': '1.html', 'identificador': '0' });
+items.push({ 'titulo': 'BOAS-VINDAS!', 'nome': 'Aula 2', 'arquivo': '2.html', 'identificador': '1' });
+items.push({ 'titulo': 'INFECÇÃO EM FERIDAS AGUDAS E CRÔNICAS', 'nome': 'Aula 3', 'arquivo': '3.html', 'identificador': '2' });
+items.push({ 'titulo': '', 'nome': 'Aula 4', 'arquivo': '4.html', 'identificador': '3' });
+items.push({ 'titulo': '', 'nome': 'Aula 5', 'arquivo': '5.html', 'identificador': '4' });
+items.push({ 'titulo': '', 'nome': 'Aula 6', 'arquivo': '6.html', 'identificador': '5' });
+items.push({ 'titulo': '', 'nome': 'Aula 7', 'arquivo': '7.html', 'identificador': '6' });
+items.push({ 'titulo': '', 'nome': 'Aula 8', 'arquivo': '8.html', 'identificador': '7' });
+items.push({ 'titulo': '', 'nome': 'Aula 9', 'arquivo': '9.html', 'identificador': '8' });
+items.push({ 'titulo': 'SINAIS DE INFECÇÃO EM FERIDAS AGUDAS', 'nome': 'Aula 10', 'arquivo': '10.html', 'identificador': '9' });
+items.push({ 'titulo': '', 'nome': 'Aula 11', 'arquivo': '11.html', 'identificador': '10' });
+items.push({ 'titulo': 'SINAIS DE INFECÇÃO EM FERIDAS CRÔNICAS', 'nome': 'Aula 12', 'arquivo': '12.html', 'identificador': '11' });
+items.push({ 'titulo': '', 'nome': 'Aula 13', 'arquivo': '13.html', 'identificador': '12' });
+items.push({ 'titulo': 'O QUE PRECISAMOS SABER SOBRE COLETA DE EXAMES MICROBIOLÓGICOS EM FERIDAS?', 'nome': 'Aula 14', 'arquivo': '14.html', 'identificador': '13' });
+items.push({ 'titulo': '', 'nome': 'Aula 15', 'arquivo': '15.html', 'identificador': '14' });
+items.push({ 'titulo': '', 'nome': 'Aula 16', 'arquivo': '16.html', 'identificador': '15' });
+items.push({ 'titulo': 'TERAPIA TÓPICA PARA FERIDAS INFECTADAS', 'nome': 'Aula 17', 'arquivo': '17.html', 'identificador': '16' });
+items.push({ 'titulo': '', 'nome': 'Aula 18', 'arquivo': '18.html', 'identificador': '17' });
+items.push({ 'titulo': 'PLANTAS MEDICINAIS E FITOTERÁPICOS', 'nome': 'Aula 19', 'arquivo': '19.html', 'identificador': '18' });
+items.push({ 'titulo': '', 'nome': 'Aula 20', 'arquivo': '20.html', 'identificador': '19' });
+items.push({ 'titulo': 'AVALIAÇÃO', 'nome': 'Aula 21', 'arquivo': '21.html', 'identificador': '20' });
+items.push({ 'titulo': 'REFERÊNCIAS', 'nome': 'Aula 22', 'arquivo': '22.html', 'identificador': '21' });
 
 
 
-items.push({ 'nome': 'Credito', 'arquivo': 'credito1.html', 'identificador': 'c1'});
-items.push({ 'nome': 'Credito', 'arquivo': 'credito2.html', 'identificador': 'c2'});
+items.push({ 'titulo': 'FICHA TÉCNICA', 'nome': 'Credito', 'arquivo': 'credito1.html', 'identificador': 'c1' });
+items.push({ 'titulo': '', 'nome': 'Credito', 'arquivo': 'credito2.html', 'identificador': 'c2' });
 
 
 
