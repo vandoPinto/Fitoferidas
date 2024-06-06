83cc8590b5b3173669873e1c20e6fe543ec2d57a
Vando Pinto <vandopintoo@gmail.com>
2024-06-06 10:12:17 -0300

    módulo 2 aula 1

--- et2/PROT-E2-U1-A1.1/js/funcoes.js
+++ et2/PROT-E2-U1-A1.1/js/funcoes.js
@@ -3,6 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+var visitadas = [];
+
 function getSearchParams(k) {
     var p = {};
     location.search.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (s, k, v) {
@@ -56,8 +59,9 @@ function montaMenu() {
     retornoNav = '';
     retornoMenu = '';
     items.forEach(function (item, indice, array) {
-        retornoMenu += '<a href=# class="alteraAula" ident=' + indice + ' >' + item.nome + '</a>';
-        retornoNav += '<i id="aA' + indice + '" class="alteraAula timeline--inner-pin  is-normal" ident=' + indice + ' data-title="' + item.nome + '"></i>';
+        if (item.titulo)
+            retornoMenu += '<a href=# class="alteraAula" ident=' + indice + ' >' + item.titulo + '</a>';
+        retornoNav += '<i id="aA' + indice + '" class="alteraAula timeline--inner-pin  is-normal" ident=' + indice + ' data-title="' + item.nome + '   " data-toggle="tooltip" data-placement="top" title="Tela ' + (indice + 1) + '"></i>';
     });
 
     $('#menuAulas').append(retornoMenu);
@@ -69,8 +73,8 @@ function atualizaProgresso() {
     totalitems = items.length;
     totalconcluido = conclusao.length;
     progressoatual = (totalconcluido == 0) ? 0 : Math.round((totalconcluido / totalitems) * 100);
-    if(progressoatual>100){
-        progressoatual=100;
+    if (progressoatual > 100) {
+        progressoatual = 100;
     }
     $('#classeProgress').removeClass();
     $('#classeProgress').addClass('c100 small p' + progressoatual);
@@ -84,13 +88,15 @@ function mudaAula(aula) {
     }
     aulaAtual = aula;
     atualizaProgresso();
-    $('#conteudo').load('slide/' + items[aula].arquivo + '?v3',function(){$('#dadosAula').html(nomeUnidade + "<br>" + nomeAula+"<b class='desenv'><br>Arq:"+ items[aula].arquivo+"</b>" );});
+    $('#conteudo').load('slide/' + items[aula].arquivo + '?v3', function () { $('#dadosAula').html(nomeUnidade + "<br>" + nomeAula); });
+    // $('#conteudo').load('slide/' + items[aula].arquivo + '?v3', function () { $('#dadosAula').html(nomeUnidade + "<br>" + nomeAula + "<b class='desenv'><br>Arq:" + items[aula].arquivo + "</b>"); });
     //Exibe os navegadores
-    
+
     if (aulaAtual == 0) {
         $('#btVo').hide();
         $('#btAv').show();
-    } else if (aulaAtual == items[items.length - 1].identificador) {
+        // } else if (aulaAtual == items[items.length - 1].identificador) {
+    } else if (aulaAtual == totalitems - 1) {
         $('#btVo').show();
         $('#btAv').hide();
     } else {
@@ -98,7 +104,22 @@ function mudaAula(aula) {
         $('#btAv').show();
     }
     closeNav();
-    $('.alteraAula[ident=' + aula + ']').addClass('concluido');
+
+    visitadas[aula] = true;
+    for (let i = 0; i < visitadas.length; i++) {
+        if (visitadas[i] === undefined) {
+            visitadas[i] = false;
+        }
+    }
+    visitadas.map((current, index) => {
+        if (current) {
+            $('.alteraAula').removeClass('current');
+            $('.alteraAula[ident=' + index + ']').addClass('concluido');
+        }
+    })
+    $('.alteraAula[ident=' + aulaAtual + ']').removeClass('concluido');
+
+    $('.alteraAula[ident=' + aulaAtual + ']').addClass('current');
 }
 
 function Navega(opcao) {
@@ -123,13 +144,13 @@ function closeNav() {
 
 }
 
-$(document).keydown(function(e){
-    if (e.which == 37) { 
-      Navega('volta')
-       return false;
+$(document).keydown(function (e) {
+    if (e.which == 37) {
+        Navega('volta')
+        return false;
     }
-     if (e.which == 39) { 
-      Navega('avanca')
-       return false;
+    if (e.which == 39) {
+        Navega('avanca')
+        return false;
     }
 });
