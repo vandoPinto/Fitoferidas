7d09c8c8356136f8d81fe8a45238fdcc6410a707
Vando Pinto <vandopintoo@gmail.com>
2024-06-07 21:31:31 -0300

    aula 5.3

--- et2/PROT-E2-U5-A5.2/js/funcoes.js
+++ et2/PROT-E2-U5-A5.2/js/funcoes.js
@@ -61,7 +61,7 @@ function montaMenu() {
     items.forEach(function (item, indice, array) {
         if (item.titulo)
             retornoMenu += '<a href=# class="alteraAula" ident=' + indice + ' >' + item.titulo + '</a>';
-        retornoNav += '<i id="aA' + indice + '" class="alteraAula timeline--inner-pin  is-normal" ident=' + indice + ' data-title="' + item.nome + '   " data-toggle="tooltip" data-placement="top" title="Tela ' + (indice + 1) + '"></i>';
+        retornoNav += '<i id="aA' + indice + '" class="alteraAula timeline--inner-pin  is-normal" ident=' + indice + '   " data-toggle="tooltip" data-placement="top" title="Tela ' + (indice + 1) + '"></i>';
     });
 
     $('#menuAulas').append(retornoMenu);
@@ -88,7 +88,8 @@ function mudaAula(aula) {
     }
     aulaAtual = aula;
     atualizaProgresso();
-    $('#conteudo').load('slide/' + items[aula].arquivo + '?v3', function () { $('#dadosAula').html(nomeUnidade + "<br>" + nomeAula + "<b class='desenv'><br>Arq:" + items[aula].arquivo + "</b>"); });
+    $('#conteudo').load('slide/' + items[aula].arquivo + '?v3', function () { $('#dadosAula').html(nomeUnidade + "<br>" + nomeAula); });
+    // $('#conteudo').load('slide/' + items[aula].arquivo + '?v3', function () { $('#dadosAula').html(nomeUnidade + "<br>" + nomeAula + "<b class='desenv'><br>Arq:" + items[aula].arquivo + "</b>"); });
     //Exibe os navegadores
 
     if (aulaAtual == 0) {
